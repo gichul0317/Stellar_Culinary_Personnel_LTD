9c11671178313b53f7b6d1bcc1863ee1086e2257
gichul <gichul0317@gmail.com>
2023-02-25 20:48:40 -0500

    all boards working

--- src/components/KanbanBoard/KanbanBoard.js
+++ src/components/KanbanBoard/KanbanBoard.js
@@ -5,13 +5,16 @@ import { mockData } from '../../mockData/mockData';
 import { DragDropContext, Draggable } from 'react-beautiful-dnd';
 
 function KanbanBoard(props) {
+  // state for mock data
   const [items, setItems] = useState(mockData);
 
+  // drag end handler for dragging items to the different board
   const onDragEnd = () => {};
 
   return (
     <Fragment>
       <DragDropContext onDragEnd={onDragEnd}>
+        {/* Todo board */}
         <div className="col">
           <div className="card mb-4 rounded-3 shadow-sm">
             <Lists title="To Do" onDragEnd={onDragEnd} name="todo">
@@ -37,18 +40,56 @@ function KanbanBoard(props) {
             </Lists>
           </div>
         </div>
+        {/* inprogress board */}
         <div className="col">
           <div className="card mb-4 rounded-3 shadow-sm">
-            <Lists
-              title="In Progress"
-              onDragEnd={onDragEnd}
-              name="inprogress"
-            />
+            <Lists title="To Do" onDragEnd={onDragEnd} name="inprogress">
+              {items.inprogress.map((item, index) => (
+                <Draggable
+                  key={item.uuid}
+                  draggableId={item.uuid}
+                  index={index}
+                >
+                  {(provided, snapshot) => (
+                    <div>
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <Card data={item} />
+                      </div>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+            </Lists>
           </div>
         </div>
+        {/* done board */}
         <div className="col">
-          <div className="card mb-4 rounded-3 shadow-sm" name="done">
-            <Lists title="Done" onDragEnd={onDragEnd} name="done" />
+          <div className="card mb-4 rounded-3 shadow-sm">
+            <Lists title="To Do" onDragEnd={onDragEnd} name="done">
+              {items.done.map((item, index) => (
+                <Draggable
+                  key={item.uuid}
+                  draggableId={item.uuid}
+                  index={index}
+                >
+                  {(provided, snapshot) => (
+                    <div>
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <Card data={item} />
+                      </div>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+            </Lists>
           </div>
         </div>
       </DragDropContext>
